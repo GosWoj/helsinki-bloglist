c70ed440d0e2129bd6e8ab0d4492479cbf4a3106
GosWoj <gosiawoojcik@gmail.com>
2021-08-15 16:08:48 +0200

    unit testing step3 done - created test and function that searches for the most liked blog post

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,12 +3,27 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (blogs) => {
-  const total = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  const total = blogs.reduce((acc, blog) => acc + blog.likes, 0);
 
   return blogs.length === 0 ? 0 : total;
+};
+
+const favoriteBlog = (blogs) => {
+  const mostLikes = blogs.reduce((acc, blog) => {
+    return acc.likes > blog.likes ? acc : blog;
+  });
+
+  const blog = {
+    title: mostLikes.title,
+    author: mostLikes.author,
+    likes: mostLikes.likes,
+  };
+
+  return blog;
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };

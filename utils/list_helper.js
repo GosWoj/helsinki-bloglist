982d6560cc465a52fd05a7c506a5d579a66ffdf5
GosWoj <gosiawoojcik@gmail.com>
2021-08-15 15:28:51 +0200

    unit testing step2 done - tests for summary of likes are passing

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+
+  return blogs.length === 0 ? 0 : total;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };

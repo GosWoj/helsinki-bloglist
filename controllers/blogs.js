35c89de7fa26513fe83311a8d325410b74d234a8
GosWoj <gosiawoojcik@gmail.com>
2021-08-18 16:11:34 +0200

    tests step1 done - wrote test for api and refactored code to use async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,13 +2,12 @@ const blogsRoute = require("express").Router();
 const Blog = require("../models/blog");
 const logger = require("../utils/logger");
 
-blogsRoute.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRoute.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRoute.post("/", (request, response) => {
+blogsRoute.post("/", async (request, response) => {
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
@@ -16,14 +15,12 @@ blogsRoute.post("/", (request, response) => {
     likes: request.body.likes,
   });
 
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.json(savedBlog);
-    })
-    .catch((error) => {
-      logger.error(error);
-    });
+  try {
+    const savedBlog = await blog.save();
+    response.json(savedBlog);
+  } catch (exception) {
+    logger.error(exception);
+  }
 });
 
 module.exports = blogsRoute;

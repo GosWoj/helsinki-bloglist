e044a060294b843508b53556c5d0e9453e2ea22a
GosWoj <gosiawoojcik@gmail.com>
2021-08-23 16:18:57 +0200

    expansions step4 - added restrictions for username and password

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const usersRouter = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const logger = require("../utils/logger");
 
 usersRouter.get("/", async (request, response) => {
   const users = await User.find({});
@@ -9,18 +10,30 @@ usersRouter.get("/", async (request, response) => {
 });
 
 usersRouter.post("/", async (request, response) => {
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(request.body.password, saltRounds);
+  if (!request.body.password || request.body.password.length <= 3) {
+    response.status(400).json({
+      error: "Password missing or too short",
+    });
+  } else {
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(request.body.password, saltRounds);
 
-  const user = new User({
-    username: request.body.username,
-    name: request.body.name,
-    passwordHash,
-  });
+    const user = new User({
+      username: request.body.username,
+      name: request.body.name,
+      passwordHash,
+    });
 
-  const savedUser = await user.save();
-
-  response.json(savedUser);
+    try {
+      const savedUser = await user.save();
+      response.json(savedUser);
+    } catch (error) {
+      logger.error(error);
+      response.status(400).json({
+        error: "Invalid username or password",
+      });
+    }
+  }
 });
 
 module.exports = usersRouter;

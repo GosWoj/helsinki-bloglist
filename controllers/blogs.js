0d8370f6727572cccc732a4662f42f7d888c4cb9
GosWoj <gosiawoojcik@gmail.com>
2021-08-24 14:45:57 +0200

    expansions step5 done - blog contains user id and user contains blogs id, both api routes are populated with information about each other

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,13 @@
 const blogsRoute = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 const logger = require("../utils/logger");
 
 blogsRoute.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    name: 1,
+    username: 1,
+  });
   response.json(blogs);
 });
 
@@ -14,15 +18,20 @@ blogsRoute.post("/", async (request, response) => {
     });
   }
 
+  const user = await User.findById(request.body.userId);
+
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
     url: request.body.url,
     likes: request.body.likes || 0,
+    user: user._id,
   });
 
   try {
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
     response.json(savedBlog);
   } catch (exception) {
     logger.error(exception);
